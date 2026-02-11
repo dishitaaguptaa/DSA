7f4f7b80b11f3311ecbdee3b763137c47a1e2a82
dishitaagupta <dishitagupta2003@gmail.com>
2026-02-12 01:23:44 +0530

    Day9: Find the element which appears majority times(>n/2) in an Array

---
+++ II_Arrays/Medium/03_majorityElement.java
@@ -0,0 +1,58 @@
+package II_Arrays.Medium;
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int candidate = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return candidate;
+    }
+}
+
+
+
+
+/* import java.util.Arrays;
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length / 2];
+    }
+}
+
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+
+            for (int j = 0; j < n; j++) {
+                if (nums[i] == nums[j]) {
+                    count++;
+                }
+            }
+
+            if (count > n / 2) {
+                return nums[i];
+            }
+        }
+
+        return -1; // just for safety
+    }
+}
+ */
