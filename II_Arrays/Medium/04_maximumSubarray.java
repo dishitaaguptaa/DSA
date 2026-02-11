84c98fd20504d9760c78b497d229e09d44d2b225
dishitaagupta <dishitagupta2003@gmail.com>
2026-02-12 01:24:18 +0530

    Day9: Subarray that has Largest Sum

---
+++ II_Arrays/Medium/04_maximumSubarray.java
@@ -0,0 +1,15 @@
+package II_Arrays.Medium;
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            int sum = 0;
+            for (int j = i; j < nums.length; j++) {
+                sum = sum + nums[j];
+                maxSum = Math.max(sum, maxSum);
+            }
+        }
+        return maxSum;
+    }
+}
