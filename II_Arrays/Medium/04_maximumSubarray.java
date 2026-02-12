aae07bb021dad4456076990a54863db8d3a7fabb
dishitaagupta <dishitagupta2003@gmail.com>
2026-02-12 22:18:55 +0530

    Day10: Subarray which has maximum sum in an array(return max sum)

--- II_Arrays/Medium/04_maximumSubarray.java
+++ II_Arrays/Medium/04_maximumSubarray.java
@@ -1,6 +1,38 @@
 package II_Arrays.Medium;
 
+//Brute 
 class Solution {
+    public int maxSubArray(int[] nums) {
+
+        int n = nums.length;
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) { // starting index
+            for (int j = i; j < n; j++) { // ending index
+
+                int sum = 0;
+
+                for (int k = i; k <= j; k++) { // calculate sum
+                    sum += nums[k];
+                }
+
+                if (sum > maxSum) {
+                    maxSum = sum;
+                }
+            }
+        }
+
+        return maxSum;
+    }
+}
+//TC - O(N^3)
+//SC - O(1)
+
+
+
+
+//Better
+/* class Solution {
     public int maxSubArray(int[] nums) {
         int maxSum = nums[0];
         for (int i = 0; i < nums.length; i++) {
@@ -12,4 +44,27 @@ class Solution {
         }
         return maxSum;
     }
-}
+} */
+//TC - O(N^2)
+// SC - O(1);
+
+
+
+
+//Optimal
+/* class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum = sum + nums[i];
+            maxSum = Math.max(sum, maxSum);
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return maxSum;
+    }
+} */
+//TC - O(N)
+//SC - O(1)
